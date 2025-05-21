6c69cf32c1ec61e885f216474b17f29f5ac66319
om kaldate <omkaldate23@gmail.com>
2025-05-22 00:28:32 +0530

    Delete frontend and Backend folders from repository

--- Backend/connectDB/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose =  require("mongoose");
-
-const connectionDb = async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/StockTradersHub")
-    .then(()=>console.log("DB connected"));
-}
-
-
-module.exports = connectionDb;
