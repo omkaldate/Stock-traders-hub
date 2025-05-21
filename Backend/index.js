923294db65ee88184745d1fd4251ecf98e391d15
om kaldate <omkaldate23@gmail.com>
2025-05-22 00:14:57 +0530

    stock tradesr analysis

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const route = require("./routes/stock");
 const connectionDb =  require("./connectDB/connect")
+const cors = require('cors');
+
 
 const app = express();
 const PORT = 8000;
 
 connectionDb();
 app.use(express.json());
+app.use(cors());
+
 app.use("/stock", route);
 
 
